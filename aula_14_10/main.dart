1cea9d9873b04320dbfe0cc256f225a041fe7412
Rafael Levi <you@example.com>
2025-10-14 20:33:51 -0300

    if else

--- aula_14_10/main.dart
+++ aula_14_10/main.dart
@@ -2,6 +2,11 @@ void main() {
   String nome = 'Rafael Levi';
   int idade = 10;
   double altura = 1.49;
-  bool ehlegaldemais = true;
-  print ('$nome, $idade, $altura, $ehlegaldemais');
+  bool ehMaiordeIdade = false ;
+
+  if (idade >= 18) {
+    print('você eh maior de idade');
+ } else {
+  print('você eh menor de idade');
+ }
 }   
